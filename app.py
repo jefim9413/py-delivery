6e2f9aa24005a5a58f51f6ac91a5409cd9e1ab7b
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2025-07-23 22:55:06 -0300

    fix

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 import requests
 
 app = Flask(__name__)
+ 
+app.secret_key = "sua_chave_secreta"
 
 API_BASE = "https://delivery-api-i9pg.onrender.com"
 
